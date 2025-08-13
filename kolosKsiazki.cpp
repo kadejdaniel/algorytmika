4092860e4f53ead5d42960e99b8ae1d001cde72d
daneq <77daniel.kontakt@gmail.com>
2025-08-13 16:02:01 +0200

    Usuń cmake-build-debug z repozytorium

--- kolosKsiazki.cpp
+++ kolosKsiazki.cpp
@@ -1,3 +1,40 @@
-//
-// Created by daneq on 13.08.2025.
-//
+#include <iostream>
+using namespace std;
+
+const int n = 4;
+
+struct ksiazka {
+    string tytul;
+    int rok;
+};
+
+void insertionSort(ksiazka tab[]) {
+    int i = n - 2;
+    while (i >= 0) {
+        int j = i;
+        ksiazka temp = tab[j];
+        while (temp.rok > tab[j + 1].rok && j < n - 1) {
+            tab[j].rok = tab[j + 1].rok;
+            j = j + 1;
+        }
+        tab[j].rok = temp.rok;
+        i = i - 1;
+    }
+}
+
+void wyswietl(ksiazka tab[]) {
+    for (int i = 0; i < n; i++) {
+        cout << "Tytul to: " << tab[i].tytul << " " << "Rok wydania: " << tab[i].rok << endl;
+    }
+}
+
+int main() {
+    ksiazka tab[n] = {
+        {"mis", 2007}, {"amis", 2010}, {"bmis", 2006}, {"cmis", 1999},
+    };
+    wyswietl(tab);
+    insertionSort(tab);
+    cout << "po sortowaniu: " << endl;
+    wyswietl(tab);
+    return 0;
+}
